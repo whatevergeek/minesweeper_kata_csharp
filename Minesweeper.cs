b2cc2254659d87629a1a140f7fc55e64f3c9dcd4
Paul Amazona <whatevergeek@gmail.com>
2020-12-19 20:56:38 +0800

    basic implementation

--- Minesweeper.cs
+++ Minesweeper.cs
@@ -1,11 +1,68 @@
 using System;
+using System.Collections.Generic;
 
 namespace minesweeper_kata_csharp
 {
     public class Minesweeper
     {
-        public static int[][] SweepMineField(int[][] mineField)
+        public static int[,] SweepMineField(int[,] mineField)
         {
+            // get mine locations
+            var mineLocations = GetMineLocations(mineField);
+
+            // foreach mine location increment adjacent cells
+            foreach (var location in mineLocations)
+            {
+                mineField = IncrementAdjacentCells(location, mineField);
+            }
+
+            return mineField;
+        }
+
+        private static List<(int Row, int Column)> GetMineLocations(int[,] mineField)
+        {
+            List<(int, int)> mineLocations = new List<(int Row, int Column)>();
+
+            for (int row = 0; row < mineField.GetLength(0); row++)
+                for (int col = 0; col < mineField.GetLength(1); col++)
+                {
+                    if (mineField[row, col] == -1)
+                        mineLocations.Add((row, col));
+                }
+
+            return mineLocations;
+        }
+
+        private static int[,] IncrementAdjacentCells((int Row, int Column) location, int[,] mineField)
+        {
+            var maxRow = mineField.GetLength(0);
+            var maxCol = mineField.GetLength(1);
+
+            // top left
+            if (location.Row > 0 && location.Column > 0 && mineField[location.Row - 1, location.Column - 1] > -1)
+                mineField[location.Row - 1, location.Column - 1]++;
+            // top 
+            if (location.Row > 0 && mineField[location.Row - 1, location.Column] > -1)
+                mineField[location.Row - 1, location.Column]++;
+            // top right
+            if (location.Row > 0 && location.Column < maxCol - 1 && mineField[location.Row - 1, location.Column + 1] > -1)
+                mineField[location.Row - 1, location.Column + 1]++;
+            // left 
+            if (location.Column > 0 && mineField[location.Row, location.Column - 1] > -1)
+                mineField[location.Row, location.Column - 1]++;
+            // right
+            if (location.Column < maxCol - 1 && mineField[location.Row, location.Column + 1] > -1)
+                mineField[location.Row, location.Column + 1]++;
+            // bottom left
+            if (location.Row < maxRow - 1 && location.Column > 0 && mineField[location.Row + 1, location.Column - 1] > -1)
+                mineField[location.Row + 1, location.Column - 1]++;
+            // bottom 
+            if (location.Row < maxRow - 1 && mineField[location.Row + 1, location.Column] > -1)
+                mineField[location.Row + 1, location.Column]++;
+            // bottom right
+            if (location.Row < maxRow - 1 && location.Column < maxCol - 1 && mineField[location.Row + 1, location.Column + 1] > -1)
+                mineField[location.Row + 1, location.Column + 1]++;
+
             return mineField;
         }
     }
--- MinesweeperTestPack.cs
+++ MinesweeperTestPack.cs
@@ -8,18 +8,94 @@ namespace minesweeper_kata_csharp
         [Fact]
         public void TestEmptyMineField()
         {
-            var emptyMineField = new int[][]
+            var emptyMineField = new int[,]
             {
-                new int[] {0,0,0},
-                new int[] {0,0,0},
-                new int[] {0,0,0}
+                {0,0,0},
+                {0,0,0},
+                {0,0,0}
             };
 
             var actual = Minesweeper.SweepMineField(emptyMineField);
 
             for (int row = 0; row < 3; row++)
                 for (int col = 0; col < 3; col++)
-                    Assert.Equal(0, actual[row][col]);
+                    Assert.Equal(0, actual[row, col]);
+        }
+
+        [Fact]
+        public void TestOneMine()
+        {
+            var inputMineField = new int[,]
+            {
+                {0,-1,0},
+                {0,0,0},
+                {0,0,0}
+            };
+
+            var actualMineField = Minesweeper.SweepMineField(inputMineField);
+
+            var expectedMineField = new int[,]
+            {
+                {1,-1,1},
+                {1,1,1},
+                {0,0,0}
+            };
+
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
+                    Assert.Equal(expectedMineField[row, col], actualMineField[row, col]);
+        }
+
+        [Fact]
+        public void TestTwoMines()
+        {
+            var inputMineField = new int[,]
+            {
+                {0,-1,0},
+                {0,0,0},
+                {0,0,-1}
+            };
+
+            var actualMineField = Minesweeper.SweepMineField(inputMineField);
+
+            var expectedMineField = new int[,]
+            {
+                {1,-1,1},
+                {1,2,2},
+                {0,1,-1}
+            };
+
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
+                    Assert.Equal(expectedMineField[row, col], actualMineField[row, col]);
+        }
+
+        [Fact]
+        public void Test4Mines5x6Grid()
+        {
+            var inputMineField = new int[,]
+            {
+                {0,-1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0},
+                {0, 0,-1, 0, 0, 0},
+                {0, 0, 0,-1, 0, 0},
+                {0,-1, 0, 0, 0, 0}
+            };
+
+            var actualMineField = Minesweeper.SweepMineField(inputMineField);
+
+            var expectedMineField = new int[,]
+            {
+                {1,-1, 1, 0, 0, 0},
+                {1, 2, 2, 2, 0, 0},
+                {0, 1,-1, 2, 1, 0},
+                {1, 2, 3,-1, 1, 0},
+                {1,-1, 2, 1, 1, 0}
+            };
+
+            for (int row = 0; row < 3; row++)
+                for (int col = 0; col < 3; col++)
+                    Assert.Equal(expectedMineField[row, col], actualMineField[row, col]);
         }
     }
 }
